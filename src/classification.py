b0e390e3132f81c2f5bf6911dc2e65bbfebd2b76
gdalessi <g.dls@yahoo.com>
2020-04-23 00:07:35 +0200

    try push to forked Github

---
+++ src/classification.py
@@ -0,0 +1,17 @@
+'''
+MODULE: classification.py
+
+@Authors:
+    G. D'Alessio [1,2]
+    [1]: Université Libre de Bruxelles, Aero-Thermo-Mechanics Laboratory, Bruxelles, Belgium
+    [2]: CRECK Modeling Lab, Department of Chemistry, Materials and Chemical Engineering, Politecnico di Milano
+
+@Contacts:
+    giuseppe.dalessio@ulb.ac.be
+
+
+@Additional notes:
+    This code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+    Please report any bug to: giuseppe.dalessio@ulb.ac.be
+
+'''
